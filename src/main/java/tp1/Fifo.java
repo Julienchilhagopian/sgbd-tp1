398033e69c08d88856f1c566a28faf682570ae4a
Julienchilhagopian <julienchilhagopian@gmail.com>
2020-11-20 10:17:32 +0100

    fifo

--- src/main/java/tp1/Fifo.java
+++ src/main/java/tp1/Fifo.java
@@ -3,25 +3,38 @@ package tp1;
 import java.util.LinkedList;
 
 public class Fifo {
+    private LinkedList<String> index = new LinkedList<>();
+    private LinkedList<String> fifo = new LinkedList<>();
+    private int page_miss = 0;
 
 
-
-    public void FIFO(LinkedList<String> pageSequence, int capacity) {
-        LinkedList index = new LinkedList();
-        LinkedList fifo = new LinkedList<String>();
-
-
-        int page_miss = 0;
-
+    public void start(LinkedList<String> pageSequence, int capacity) {
         for(String page : pageSequence) {
             if (fifo.size() < capacity) {
-                if(!fifo.contains(page)){
-                    
+                if (!fifo.contains(page)){
+                    fifo.add(page);
+                    page_miss++;
+                    index.add(page);
                 }
+            } else {
+                String val = index.get(0);
+                index.removeFirst();
+
+                fifo.remove(val);
+                fifo.add(page);
+                index.add(page);
+                page_miss++;
             }
+
         }
 
+    }
 
+    public int getPage_miss() {
+        return page_miss;
+    }
 
+    public LinkedList<String> getFifo() {
+        return fifo;
     }
 }
--- src/main/java/tp1/Main.java
+++ src/main/java/tp1/Main.java
@@ -1,7 +1,16 @@
 package tp1;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
+        LinkedList<String> letters = new LinkedList<>(Arrays.asList("A", "B", "C", "D", "E", "A", "B", "C", "D", "E"));
 
+        Fifo fifo = new Fifo();
+        fifo.start(letters, 4);
+
+        System.out.println(fifo.getFifo());
+        System.out.println(fifo.getPage_miss());
     }
 }
